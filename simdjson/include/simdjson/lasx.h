c64569528b610704d9602e7a0f1b0ee7382d7e71
Pradumn Shukla <pradumn.eng01@gmail.com>
2024-07-16 18:37:02 +0200

    cleanup the code

--- simdjson/include/simdjson/lasx.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMDJSON_LASX_H
-#define SIMDJSON_LASX_H
-
-#include "simdjson/lasx/begin.h"
-#include "simdjson/generic/amalgamated.h"
-#include "simdjson/lasx/end.h"
-
-#endif // SIMDJSON_LASX_H
--- simdjson/include/simdjson/lsx.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMDJSON_LSX_H
-#define SIMDJSON_LSX_H
-
-#include "simdjson/lsx/begin.h"
-#include "simdjson/generic/amalgamated.h"
-#include "simdjson/lsx/end.h"
-
-#endif // SIMDJSON_LSX_H
